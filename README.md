e5d192c08245b4698878e1e45e87303a1015e14b
nicobarragan_@hotmail.com <nicobarragan_@hotmail.com>
2024-12-31 01:49:40 -0300

    Adding readme, grub, lvim , enabling sddm

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fresh Install Script
+I'm too lazy to search for every single package that I need so I created a script to run on a fresh install.
+
+## Installed Programs
+| Type | Name |
+| Terminal Emulator | [Wezterm](https://wezfurlong.org/wezterm/index.html) |
+| Terminal | [Z shell](https://www.zsh.org) |
+| Terminal Framework | [Oh-My-Zsh](https://ohmyz.sh) |
+| IDE | [LunarVim](https://www.lunarvim.org) |
+| Browser | [Vivaldi](https://vivaldi.com) |
+| Window Manager | [Qtile](https://qtile.org) |
+| Display Server | [X11](https://www.x.org/wiki/) |
+| Compositor | [Picom](https://github.com/yshui/picom) |
+| Status Bar | [Polybar](https://github.com/polybar/polybar) |
+
+
--- freshinstaller.sh
+++ freshinstaller.sh
@@ -36,18 +36,28 @@ sudo pacman -S --needed \
   xorg dmenu rofi qtile polybar vivaldi neovim vim wmctrl wezterm \
   ttf-firacode-nerd noto-fonts-cjk playerctl rustup \
   python-pynvim python-distutils-extra spotify-launcher \
-  at-spi2-core libxdamage
+  at-spi2-core libxdamage xorg-server
 
 yay -S --needed --answerclean a --answerdiff n \
   zscroll
 
 # INFO: This part installs OH-MY-ZSH and its plugins.
-git clone https://github.com/ohmyzsh/ohmyzsh.git $EXTRADIR/oh-my-zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-  ${ZSH_CUSTOM:-$EXTRADIR/oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
-  ${ZSH_CUSTOM:-$EXTRADIR/oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-  chsh -s $(which zsh)
+
+if [ -z "$EXTRADIR/oh-my-zsh" ]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git $EXTRADIR/oh-my-zsh
+fi
+
+if [ -z "$EXTRADIR/oh-my-zsh/custom/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    ${ZSH_CUSTOM:-$EXTRADIR/oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+if [ -z "$EXTRADIR/oh-my-zsh/custom/plugins/fast-syntax-highlighting" ]; then 
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+    ${ZSH_CUSTOM:-$EXTRADIR/oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+fi
+
+chsh -s $(which zsh)
 
 # INFO: This part will install Node Version Manager
 export NVM_DIR="$EXTRADIR/nvm" && (
@@ -60,5 +70,12 @@ export NVM_DIR="$EXTRADIR/nvm" && (
 rustup install stable
 nvm install --lts
 
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+sudo ln -sr $LOCALBIN/lvim /usr/bin
+
+# INFO: Enables SDDM service to ensure a good account management.
 sudo systemctl enable sddm
+
+# INFO: Install GRUB (You should make your config later if default is not enough)
+sudo grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 
